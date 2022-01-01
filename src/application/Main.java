35f3c8185ae4f52228b16a3476847b75744021ef
jungsukim1 <jungsukim2609@gmail.com>
2022-01-01 01:09:07 -0700

    added the toggle buttons for walls, start node, end node

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,55 +3,61 @@ package application;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	private int WINDOWHEIGHT = 800; 
-	private int WINDOWWIDTH = 800;
-	private int NUM_BUTTON_LINES = 25;
-	private int BUTTONS_PER_LINE = 20;
+	private int WINDOWWIDTH = 900;
+	private int NUM_BUTTON_LINES = 32;
+	private int BUTTONS_PER_LINE = 32;
 	
 	private GridPane MAP = new GridPane();
-	private Label LBLWALLS = new Label("Walls");
-	private Label LBLSTARTNODE = new Label("Start Node");
-	private Label LBLENDNODE = new Label("End Node");
-	private RadioButton ENDNODE = new RadioButton();
-	private RadioButton STARTNODE = new RadioButton();
-	private RadioButton WALLS = new RadioButton();
+	private RadioButton ENDNODE = new RadioButton("End Node");
+	private RadioButton STARTNODE = new RadioButton("Start Node");
+	private RadioButton WALLS = new RadioButton("Walls");
 	
 	
 	@Override
 	public void start(Stage primaryStage) {
 		Pane root = new Pane();
-		root.getChildren().add(MAP);
-		addButton();
-		Scene scene = new Scene(root, WINDOWWIDTH, WINDOWHEIGHT);
+		root.setPrefSize(WINDOWWIDTH, WINDOWHEIGHT);
+		VBox toggle = new VBox();
+		ToggleGroup group = new ToggleGroup();
+		ENDNODE.setToggleGroup(group);
+		STARTNODE.setToggleGroup(group);
+		WALLS.setToggleGroup(group);
+		toggle.getChildren().addAll(ENDNODE,STARTNODE,WALLS);
+		toggle.setSpacing(50);
+		toggle.setAlignment(Pos.CENTER_LEFT);
+		toggle.relocate(820, 350);
+		Parent map = createMap();
+		root.getChildren().addAll(map, toggle);
+		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
-	private void addButton() {
-//        Button b = new Button("b");
-//        b.setTranslateX(-300 + 25);
-//		b.setTranslateY(-200 + 25);
-//		b.setPrefSize(25, 25);
-//        MAP.add(b);
-		
-		
+	private Parent createMap() {
+		GridPane main = new GridPane();
         for (int r = 0; r < NUM_BUTTON_LINES; r++) {
             for (int c = 0; c < BUTTONS_PER_LINE; c++) {
                 //int number = NUM_BUTTON_LINES * r + c;
-                Button button = new Button();            	
-                button.setPrefSize(30, 30);
-                MAP.add(button, c, r);
+                Button button = new Button(); 
+                button.setPrefSize(25, 20);
+                main.add(button, c, r);
                 button.setId(r+","+c);
                 button.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
                     @Override
@@ -62,7 +68,9 @@ public class Main extends Application {
                 });
             }
         }
+        return main;
     }
+	
 	
 	public static void main(String[] args) {
 		launch(args);
